6e5f845ee2f2326118d7eab73c3dcd8e0060407b
Arcob <1241994554@qq.com>
2019-01-19 19:13:12 +0800

    add

---
+++ 09_QueueWithTwoStacks/09_QueueWithTwoStacks.cpp
@@ -0,0 +1,43 @@
+﻿// 09_QueueWithTwoStacks.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include "pch.h"
+#include "Queue.h"
+
+// ====================测试代码====================
+void Test(char actual, char expected)
+{
+	if (actual == expected)
+		printf("Test passed.\n");
+	else
+		printf("Test failed.\n");
+}
+
+int main(int argc, char* argv[])
+{
+	CQueue<char> queue;
+
+	queue.appendTail('a');
+	queue.appendTail('b');
+	queue.appendTail('c');
+
+	char head = queue.deleteHead();
+	Test(head, 'a');
+
+	head = queue.deleteHead();
+	Test(head, 'b');
+
+	queue.appendTail('d');
+	head = queue.deleteHead();
+	Test(head, 'c');
+
+	queue.appendTail('e');
+	head = queue.deleteHead();
+	Test(head, 'd');
+
+	head = queue.deleteHead();
+	Test(head, 'e');
+
+	return 0;
+}
+
---
+++ 10_Fibonacci/10_Fibonacci.cpp
@@ -0,0 +1,53 @@
+﻿// 10_Fibonacci.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include "pch.h"
+#include <iostream>
+
+long long Fibonacci_Solution(unsigned int n)
+{
+	if (n == 0) {
+		return 0;
+	}
+	else if (n == 1) {
+		return 1;
+	}
+	long long result[] = { 0,1 };
+	unsigned int index = 1;
+	long long temp = 0;
+	while (index < n) {
+		temp = result[0] + result[1];
+		result[0] = result[1];
+		result[1] = temp;
+		index++;
+	}
+	return temp;
+}
+
+// ====================测试代码====================
+void Test(int n, int expected)
+{
+	if (Fibonacci_Solution(n) == expected)
+		printf("Test for %d in solution1 passed.\n", n);
+	else
+		printf("Test for %d in solution1 failed.\n", n);
+}
+
+int main(int argc, char* argv[])
+{
+	Test(0, 0);
+	Test(1, 1);
+	Test(2, 1);
+	Test(3, 2);
+	Test(4, 3);
+	Test(5, 5);
+	Test(6, 8);
+	Test(7, 13);
+	Test(8, 21);
+	Test(9, 34);
+	Test(10, 55);
+
+	Test(40, 102334155);
+
+	return 0;
+}
