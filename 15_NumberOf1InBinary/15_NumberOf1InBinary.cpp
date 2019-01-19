1e51bf6131e0a36ce311946ba25c8a6836078065
Arcob <1241994554@qq.com>
2019-01-19 23:47:01 +0800

    add

---
+++ 15_NumberOf1InBinary/15_NumberOf1InBinary.cpp
@@ -0,0 +1,53 @@
+﻿// 15_NumberOf1InBinary.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include "pch.h"
+#include <iostream>
+
+int NumberOf1_Solution(int n)
+{
+	int count = 0;
+
+	while (n)
+	{
+		++count;
+		n = (n - 1) & n;
+	}
+
+	return count;
+}
+
+// ====================测试代码====================
+void Test(int number, unsigned int expected)
+{
+	int actual = NumberOf1_Solution(number);
+	if (actual == expected)
+		printf("Solution1: Test for %p passed.\n", number);
+	else
+		printf("Solution1: Test for %p failed.\n", number);
+
+	printf("\n");
+}
+
+int main(int argc, char* argv[])
+{
+	// 输入0，期待的输出是0
+	Test(0, 0);
+
+	// 输入1，期待的输出是1
+	Test(1, 1);
+
+	// 输入10，期待的输出是2
+	Test(10, 2);
+
+	// 输入0x7FFFFFFF，期待的输出是31
+	Test(0x7FFFFFFF, 31);
+
+	// 输入0xFFFFFFFF（负数），期待的输出是32
+	Test(0xFFFFFFFF, 32);
+
+	// 输入0x80000000（负数），期待的输出是1
+	Test(0x80000000, 1);
+
+	return 0;
+}
