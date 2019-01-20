56f6bf7750e98e1fe21e43e32afb358f1bdfba10
Arcob <1241994554@qq.com>
2019-01-20 23:11:53 +0800

    add 1

---
+++ 17_Print1ToMaxOfNDigits/17_Print1ToMaxOfNDigits.cpp
@@ -0,0 +1,61 @@
+﻿// 17_Print1ToMaxOfNDigits.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include "pch.h"
+#include <iostream>
+#include <memory>
+
+void PrintNumber(char* number);
+bool Increment(char* number);
+void Print1ToMaxOfNDigitsRecursively(char* number, int length, int index);
+
+//不会 之后看
+
+// ====================方法一====================
+void Print1ToMaxOfNDigits(int n)
+{
+	
+}
+
+// ====================公共函数====================
+// 字符串number表示一个数字，数字有若干个0开头
+// 打印出这个数字，并忽略开头的0
+void PrintNumber(char* number)
+{
+	bool isBeginning0 = true;
+	int nLength = strlen(number);
+
+	for (int i = 0; i < nLength; ++i)
+	{
+		if (isBeginning0 && number[i] != '0')
+			isBeginning0 = false;
+
+		if (!isBeginning0)
+		{
+			printf("%c", number[i]);
+		}
+	}
+
+	printf("\t");
+}
+
+// ====================测试代码====================
+void Test(int n)
+{
+	printf("Test for %d begins:\n", n);
+
+	Print1ToMaxOfNDigits(n);
+
+	printf("\nTest for %d ends.\n", n);
+}
+
+int main(int argc, char* argv[])
+{
+	Test(1);
+	Test(2);
+	Test(3);
+	Test(0);
+	Test(-1);
+
+	return 0;
+}
---
+++ 18_01_DeleteNodeInList/18_01_DeleteNodeInList.cpp
@@ -0,0 +1,133 @@
+﻿// 18_01_DeleteNodeInList.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include "pch.h"
+#include <iostream>
+#include "List.h"
+
+void DeleteNode(ListNode** pListHead, ListNode* pToBeDeleted)
+{
+	if (pListHead == nullptr || *pListHead == nullptr || pToBeDeleted == nullptr) {
+		return;
+	}
+
+	if (pToBeDeleted->m_pNext == nullptr) {
+		ListNode* temp = *pListHead;
+		if (temp == pToBeDeleted) {
+			delete pToBeDeleted;
+			pToBeDeleted = nullptr;
+			*pListHead = nullptr;
+			return;
+		}
+		while (temp->m_pNext != pToBeDeleted) {
+			temp = temp->m_pNext;
+		}
+		temp->m_pNext = nullptr;
+		delete pToBeDeleted;
+		pToBeDeleted = nullptr;
+	}
+	else {
+		ListNode* tempNext = pToBeDeleted->m_pNext;
+		pToBeDeleted->m_nValue = tempNext->m_nValue;
+		pToBeDeleted->m_pNext = tempNext->m_pNext;
+		delete tempNext;
+		tempNext = nullptr;
+	}
+}
+
+// ====================测试代码====================
+void Test(ListNode* pListHead, ListNode* pNode)
+{
+	printf("The original list is: \n");
+	PrintList(pListHead);
+
+	printf("The node to be deleted is: \n");
+	PrintListNode(pNode);
+
+	DeleteNode(&pListHead, pNode);
+
+	printf("The result list is: \n");
+	PrintList(pListHead);
+}
+
+// 链表中有多个结点，删除中间的结点
+void Test1()
+{
+	ListNode* pNode1 = CreateListNode(1);
+	ListNode* pNode2 = CreateListNode(2);
+	ListNode* pNode3 = CreateListNode(3);
+	ListNode* pNode4 = CreateListNode(4);
+	ListNode* pNode5 = CreateListNode(5);
+
+	ConnectListNodes(pNode1, pNode2);
+	ConnectListNodes(pNode2, pNode3);
+	ConnectListNodes(pNode3, pNode4);
+	ConnectListNodes(pNode4, pNode5);
+
+	Test(pNode1, pNode3);
+
+	DestroyList(pNode1);
+}
+
+// 链表中有多个结点，删除尾结点
+void Test2()
+{
+	ListNode* pNode1 = CreateListNode(1);
+	ListNode* pNode2 = CreateListNode(2);
+	ListNode* pNode3 = CreateListNode(3);
+	ListNode* pNode4 = CreateListNode(4);
+	ListNode* pNode5 = CreateListNode(5);
+
+	ConnectListNodes(pNode1, pNode2);
+	ConnectListNodes(pNode2, pNode3);
+	ConnectListNodes(pNode3, pNode4);
+	ConnectListNodes(pNode4, pNode5);
+
+	Test(pNode1, pNode5);
+
+	DestroyList(pNode1);
+}
+
+// 链表中有多个结点，删除头结点
+void Test3()
+{
+	ListNode* pNode1 = CreateListNode(1);
+	ListNode* pNode2 = CreateListNode(2);
+	ListNode* pNode3 = CreateListNode(3);
+	ListNode* pNode4 = CreateListNode(4);
+	ListNode* pNode5 = CreateListNode(5);
+
+	ConnectListNodes(pNode1, pNode2);
+	ConnectListNodes(pNode2, pNode3);
+	ConnectListNodes(pNode3, pNode4);
+	ConnectListNodes(pNode4, pNode5);
+
+	Test(pNode1, pNode1);
+
+	DestroyList(pNode1);
+}
+
+// 链表中只有一个结点，删除头结点
+void Test4()
+{
+	ListNode* pNode1 = CreateListNode(1);
+
+	Test(pNode1, pNode1);
+}
+
+// 链表为空
+void Test5()
+{
+	Test(nullptr, nullptr);
+}
+
+int main(int argc, char* argv[])
+{
+	Test1();
+	Test2();
+	Test3();
+	Test4();
+	Test5();
+
+	return 0;
+}
