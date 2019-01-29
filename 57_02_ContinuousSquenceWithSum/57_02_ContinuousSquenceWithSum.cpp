fa2a13a5a375a256a7c70ba2908ec6242da4097e
Arcob <1241994554@qq.com>
2019-01-30 00:02:46 +0800

    add

---
+++ 57_02_ContinuousSquenceWithSum/57_02_ContinuousSquenceWithSum.cpp
@@ -0,0 +1,71 @@
+﻿// 57_02_ContinuousSquenceWithSum.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include "pch.h"
+#include <iostream>
+// 面试题57（二）：为s的连续正数序列
+// 题目：输入一个正数s，打印出所有和为s的连续正数序列（至少含有两个数）。
+// 例如输入15，由于1+2+3+4+5=4+5+6=7+8=15，所以结果打印出3个连续序列1～5、
+// 4～6和7～8。
+
+#include <cstdio>
+
+void PrintContinuousSequence(int small, int big);
+
+void FindContinuousSequence(int sum)
+{
+	if (sum < 3) {
+		return;
+	}
+
+	int slowPtr = 1;
+	int fastPtr = 2;
+	int tempSum = 3;
+
+	while (slowPtr <= (sum >> 1) && fastPtr < sum && slowPtr < fastPtr) {
+		if (tempSum == sum) {
+			PrintContinuousSequence(slowPtr, fastPtr);
+			tempSum -= slowPtr;
+			slowPtr++;
+			fastPtr++;
+			tempSum += fastPtr;
+		}
+		else if (tempSum > sum) {
+			tempSum -= slowPtr;
+			slowPtr++;
+		}
+		else {
+			fastPtr++;
+			tempSum += fastPtr;
+		}
+	}
+}
+
+void PrintContinuousSequence(int small, int big)
+{
+	for (int i = small; i <= big; ++i)
+		printf("%d ", i);
+
+	printf("\n");
+}
+
+// ====================测试代码====================
+void Test(const char* testName, int sum)
+{
+	if (testName != nullptr)
+		printf("%s for %d begins: \n", testName, sum);
+
+	FindContinuousSequence(sum);
+}
+
+int main(int argc, char* argv[])
+{
+	Test("test1", 1);
+	Test("test2", 3);
+	Test("test3", 4);
+	Test("test4", 9);
+	Test("test5", 15);
+	Test("test6", 100);
+
+	return 0;
+}
---
+++ 60_DicesProbability/60_DicesProbability.cpp
@@ -0,0 +1,129 @@
+﻿// 60_DicesProbability.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+// 面试题60：n个骰子的点数
+// 题目：把n个骰子扔在地上，所有骰子朝上一面的点数之和为s。输入n，打印出s
+// 的所有可能的值出现的概率。
+
+#include "pch.h"
+#include <iostream>
+#include <cstdio>
+#include <math.h>
+
+int g_maxValue = 6;
+
+// ====================方法一====================
+void Probability(int number, int* pProbabilities);
+void Probability(int original, int current, int sum, int* pProbabilities);
+
+void PrintProbability_Solution1(int number)
+{
+	if (number < 1)
+		return;
+
+	int maxSum = number * g_maxValue;
+	int* pProbabilities = new int[maxSum - number + 1];
+	for (int i = number; i <= maxSum; ++i)
+		pProbabilities[i - number] = 0;
+
+	Probability(number, pProbabilities);
+
+	int total = pow((double)g_maxValue, number);
+	for (int i = number; i <= maxSum; ++i)
+	{
+		double ratio = (double)pProbabilities[i - number] / total;
+		printf("%d: %e\n", i, ratio);
+	}
+
+	delete[] pProbabilities;
+}
+
+void Probability(int number, int* pProbabilities)
+{
+	for (int i = 1; i <= g_maxValue; ++i)
+		Probability(number, number, i, pProbabilities);
+}
+
+void Probability(int original, int current, int sum,
+	int* pProbabilities)
+{
+	if (current == 1)
+	{
+		pProbabilities[sum - original]++;
+	}
+	else
+	{
+		for (int i = 1; i <= g_maxValue; ++i)
+		{
+			Probability(original, current - 1, i + sum, pProbabilities);
+		}
+	}
+}
+
+// ====================方法二====================
+void PrintProbability_Solution2(int number)
+{
+	if (number < 1)
+		return;
+
+	int* pProbabilities[2];
+	pProbabilities[0] = new int[g_maxValue * number + 1]();
+	pProbabilities[1] = new int[g_maxValue * number + 1]();
+
+
+
+	int * curBuffer = pProbabilities[0];
+	int * lastBuffer = pProbabilities[1];
+
+	for (int i = 1; i <= g_maxValue; i++) {
+		lastBuffer[i] = 1;
+	}
+	
+	for (int i = 2; i <= number; i++) {
+		for (int j = i; j <= g_maxValue * i; j++) {
+			for (int k = 1; k <= 6; k++) {
+				curBuffer[j] += (j - k > 0) ? lastBuffer[j - k] : 0;
+			}
+		}
+		int* temp = curBuffer;
+		curBuffer = lastBuffer;
+		lastBuffer = temp;
+	}
+	double total = pow((double)g_maxValue, number);
+	for (int i = number; i <= g_maxValue * number; ++i)
+	{
+		double ratio = (double)lastBuffer[i] / total;
+		printf("%d: %e\n", i, ratio);
+	}
+
+	delete[] pProbabilities[0];
+	delete[] pProbabilities[1];
+
+	return;
+}
+
+// ====================测试代码====================
+void Test(int n)
+{
+	printf("Test for %d begins:\n", n);
+
+	printf("Test for solution1\n");
+	PrintProbability_Solution1(n);
+
+	printf("Test for solution2\n");
+	PrintProbability_Solution2(n);
+
+	printf("\n");
+}
+
+int main(int argc, char* argv[])
+{
+	Test(1);
+	Test(2);
+	Test(3);
+	Test(4);
+
+	Test(11);
+
+	Test(0);
+
+	return 0;
+}
